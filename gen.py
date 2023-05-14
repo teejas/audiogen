77c16431e3f3f9b1575bf1e304514f27fdd6dc07
Tejas Siripurapu <tejas97siripurapu@gmail.com>
2023-05-13 23:56:25 -0700

    add automatic download of ckpt if doesnt exist

--- gen.py
+++ gen.py
@@ -5,13 +5,13 @@ import soundfile as sf
 import argparse
 from audioldm import text_to_audio, style_transfer, super_resolution_and_inpainting, build_model, latent_diffusion
 
-ckpt_paths = {
-  "audioldm-s-full": "audioldm-s-full",
-  "audioldm-l-full": "audioldm-full-l.ckpt",
-  "audioldm-s-full-v2": "audioldm-full-s-v2.ckpt",
-  "audioldm-m-text-ft": "audioldm-m-text-ft.ckpt",
-  "audioldm-s-text-ft": "audioldm-s-text-ft.ckpt",
-  "audioldm-m-full": "audioldm-m-full.ckpt"
+ckpt_urls = {
+  "audioldm-s-full": "https://zenodo.org/record/7600541/files/audioldm-s-full.ckpt",
+  "audioldm-full-l": "https://zenodo.org/record/7698295/files/audioldm-full-l.ckpt",
+  "audioldm-full-s-v2": "https://zenodo.org/record/7698295/files/audioldm-full-s-v2.ckpt",
+  "audioldm-m-text-ft": "https://zenodo.org/record/7813012/files/audioldm-m-text-ft.ckpt",
+  "audioldm-s-text-ft": "https://zenodo.org/record/7813012/files/audioldm-s-text-ft.ckpt",
+  "audioldm-m-full": "https://zenodo.org/record/7813012/files/audioldm-m-full.ckpt"
 }
 
 def text2audio(text, duration, audio_path, guidance_scale, random_seed, n_candidates, steps):
@@ -34,7 +34,8 @@ def setup_args() -> (str, str, str):
                     prog='audiogen',
                     description='generates audio using AudioLDM and writes it to out.wav',
                     epilog='~tejas')
-  parser.add_argument('-c', '--checkpoint', default="audioldm-m-full")
+  parser.add_argument('-c', '--checkpoint', default="audioldm-m-full", 
+                      choices=["audioldm-s-full", "audioldm-full-l", "audioldm-full-s-v2", "audioldm-m-text-ft", "audioldm-s-text-ft", "audioldm-m-full"])
   parser.add_argument('-i', '--input_file', required=True)
   parser.add_argument('-o', '--output_file', default="out.wav")
 
@@ -44,8 +45,10 @@ def setup_args() -> (str, str, str):
 
 if __name__ == '__main__':
   (use_checkpoint, input_file, output_file) = setup_args()
-  ckpt_path = "./ckpt/" + ckpt_paths[use_checkpoint]
-
+  ckpt_path = "./ckpt/" + use_checkpoint + ".ckpt"
+  if not os.path.exists(ckpt_path):
+    r = requests.get(ckpt_urls[use_checkpoint], allow_redirects=True)
+    open(ckpt_path, 'wb').write(r.content)
   audioldm = build_model(ckpt_path=ckpt_path, model_name=use_checkpoint)
   sr = 16000 # sample rate
 
