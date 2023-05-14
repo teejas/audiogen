d782cbdd30d657a08e53a0bbe6a6e3258b3827a6
Tejas Siripurapu <tejas97siripurapu@gmail.com>
2023-05-13 23:20:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ sudo apt-get update -y && sudo apt-get install -y python3 python3-pip
 alias python=python3
 alias pip=pip3
 pip install -r requirements.txt
+mkdir ./ckpt
 wget https://zenodo.org/record/7813012/files/audioldm-m-full.ckpt -O ./ckpt/audioldm-m-full.ckpt
